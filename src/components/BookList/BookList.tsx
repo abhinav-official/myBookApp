1fcaacfe6a34a07c8c9a8592d8ee86ba2b372874
Abhinav <abhinav.3@icloud.com>
2024-08-14 09:01:17 +0530

    ++

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import Pagination from '~/components/Pagination/Pagination';
-import { Book } from '~/types';
 import styles from './BookList.module.scss';
-import BookModal from '~/components/BookModal/BookModal';
 import { useBooks } from '~/hooks/useBooks';
 import BookItem from '../BookItem/BookItem';
 
 
 const BookList: React.FC = () => {
-	const { books, addBook } = useBooks();
+	const { books } = useBooks();
 
 	const [currentPage, setCurrentPage] = useState(1);
-	const [isAdding, setIsAdding] = useState(false);
 	const booksPerPage = 5;
 
 	const indexOfLastBook = currentPage * booksPerPage;
@@ -20,14 +17,8 @@ const BookList: React.FC = () => {
 
 	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
-	const handleAddBook = (newBook: Book) => {
-		addBook(newBook);
-		setIsAdding(false);
-	};
-
 	return (
 		<div className={styles.bookList}>
-			<button className={styles.addBookButton} onClick={() => setIsAdding(true)}>Add Book</button>
 			{currentBooks.map(book => (
 				<BookItem
 					key={book.id}
@@ -35,11 +26,6 @@ const BookList: React.FC = () => {
 				/>
 			))}
 			<Pagination booksPerPage={booksPerPage} totalBooks={books.length} paginate={paginate} />
-			<BookModal
-				isOpen={isAdding}
-				onRequestClose={() => setIsAdding(false)}
-				onSubmit={handleAddBook}
-			/>
 		</div>
 	);
 };
